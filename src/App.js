32f7189307b9ec9f8d1e1aee784ec75a79b161d0
ayankhan8223 <ayankhan8223@gmail.com>
2023-03-01 15:12:22 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
       <div className="flex h-full">
-        <div className="w-[60%] bg-[#336cb5] h-screen">
+        <div className="w-[60%] hidden md:block bg-[#336cb5] h-screen">
           <LeftPart />
         </div>
-        <div className="w-[40%]  h-screen">
+        <div className="w-[100%] md:w-[40%]   h-screen">
           <RightPart />
         </div>
       </div>
